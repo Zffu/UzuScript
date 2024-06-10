c1b780a736af6c23a560b239b3b2ca9557268f1b
Zffu <radi0o0@outlook.com>
2024-06-10 16:19:13 +0200

    Added compiling function

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,8 +1,9 @@
+const fs = require("node:fs")
 const expressions = require("../basic/index").expressions;
 
 /**
  * Compiles the basic expression in the provided file content.
- * @param {*} content the provided file content
+ * @param {string} content the provided file content
  * @returns the compiled file content.
  */
 function compileBasicExpressions(content) {
@@ -13,4 +14,23 @@ function compileBasicExpressions(content) {
     })
 
     return content;
+}
+
+/**
+ * Compiles the file located at the provided path.
+ * @param {string} filePath 
+ */
+function compileFile(filePath) {
+    if(!fs.existsSync(filePath)) {
+        console.error("[ERROR] The file " + filePath + " could not be found!")
+    }
+    else {
+        fs.open(filePath, "r", (err, fd) => {
+            let content = fd;
+            content = compileBasicExpressions(content);
+
+
+            fs.writeFileSync(path, content);
+        })
+    }
 }
